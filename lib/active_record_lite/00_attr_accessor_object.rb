1620a75f84baa2d69374df676f3b30f9e5675818
Malcolm Carradine <malcolm.carradine@gmail.com>
2014-09-15 10:03:07 -0400

    Update 00_attr_accessor_object.rb

--- lib/active_record_lite/00_attr_accessor_object.rb
+++ lib/active_record_lite/00_attr_accessor_object.rb
@@ -1,4 +1,6 @@
 class AttrAccessorObject
+  
+  #Uses define_method to recreate the attr_reader and attr_writer methods
   def self.my_attr_accessor(*names)
     names.each do |name|
       define_method("#{name}") do
